c3ff565120339a2a2094cf8487eef010952e1ce6
Unknown <chitzinwin@gmail.com>
2017-11-03 14:27:45 -0400

    add api key

--- api.js
+++ api.js
@@ -5,3 +5,6 @@ Call to Yelp with three parameters
 Budget ($, $$, $$$), ZIP, Optional Food Genre
 Parse Response
 */
+
+const apiID = "jUv7Kj68ZHWx89jz_XYl4A";
+const apiKey = "FmnbNwpbWB0qea1BtvUWV0NUDYR6KEPsDifSDMJ3dBdjra1xeBvssPJLB0I6oKmg";
