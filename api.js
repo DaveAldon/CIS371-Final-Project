ceec59b7db1a1a0287b44e4f2e7648a194aecee5
Unknown <chitzinwin@gmail.com>
2017-11-04 15:21:48 -0400

    To fetch data from Yelp

--- api.js
+++ api.js
@@ -10,6 +10,40 @@ const apiID = "jUv7Kj68ZHWx89jz_XYl4A";
 const apiKey = "FmnbNwpbWB0qea1BtvUWV0NUDYR6KEPsDifSDMJ3dBdjra1xeBvssPJLB0I6oKmg";
 
 const apiRoot = "https://api.yelp.com";
-const apiAuth = "/oauth2/token";
+const apiToken = "/oauth2/token";
 const grantType = "client_credentials";
-const Searchapi = '/v3/businesses/search';
+const Searchapi = '/v3/businesses/search?';
+
+zip = '49504';
+
+function getFoodwithZip(zip){
+    $.ajax(`${apiRoot}${apiToken}`, {
+        type: "POST",
+        crossDomain: true,
+         data: {
+            grant_type: grantType,
+            client_id: apiID,
+            client_secret: apiKey
+        },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer $token")
+
+        },
+        complete: function(response){ console.log(response);}
+        });
+		
+//         contentType: 'application/x-www-form-urlencoded; charset=UTF-8',      
+//           dataType: "json",
+//         success: function (data) {
+//             console.log(data);
+    
+//         },
+//         error: function () {
+//             console.log("post call error");
+//         }
+    
+    
+//     })
+}
+
+getFoodwithZip(zip);
