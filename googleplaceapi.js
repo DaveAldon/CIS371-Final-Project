68b42dbd41bc5c046cde33e7a07ddd67ec721946
Unknown <chitzinwin@gmail.com>
2017-11-17 23:04:00 -0500

    Return minimum 20 per each/ In progress using google-map component

--- googleplaceapi.js
+++ googleplaceapi.js
@@ -12,10 +12,8 @@ var geocoder;
 var request;
 var detailRequest;
 var delay = 0;
-var paginate=0;
+var paginate = 0;
 //let map = new google.maps.Map(document.createElement('div'));
-
-
 
 function initAutocomplete() {
   service = new google.maps.places.PlacesService(document.createElement('div'));
@@ -36,12 +34,12 @@ function initAutocomplete() {
   searchBox.addListener('places_changed', function () {
     $("#list").empty();
     $('#table > tr').remove();
-    $('#table').append("<tr><th>Name</th><th>Rating</th><th>PriceLevel</th><th>URL</th><th>OpenNow</th><th>ID</th></tr>");
+    $('#table').append("<tr><th>Count</th><th>Name</th><th>Rating</th><th>PriceLevel</th><th>URL</th><th>OpenNow</th><th>ID</th></tr>");
     // $('#table').append("<tr><th>Name</th><th>Rating</th><th>Types</th><th>ID</th></tr>");
 
     //Testing
     var places = searchBox.getPlaces();
-    console.log(places[0].name);
+    console.log(places[0]);
     if (places.length > 0) {
       places.forEach(function (place) {
 
@@ -54,98 +52,98 @@ function initAutocomplete() {
               request = {
                 location: new google.maps.LatLng(result.geometry.location.lat(), result.geometry.location.lng()),
                 radius: '500',
-                query: 'restaurant',                
+                query: 'restaurant',
                 minPriceLevel: 0,
                 maxPriceLevel: 4
               };
-              service.textSearch(request, processResults);
-              // function callback(results, status) {                         //only return 20
-              // if (status == google.maps.places.PlacesServiceStatus.OK) {
 
-              function processResults(results, status, pagination) {     //to get up to 60 places
-                console.log(results);
-                if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                  return;
-                } else {
-                  if (pagination.hasNextPage) {
-
-
-                    console.log(results.length);
-                    //$('#list').append(`<h4> Nearest ${results.length} around given address </h4>`);
-
-
-                    for (var i = 0; i < results.length; i++) {
-                      //console.log(results[i]);
-                      //var place = results[i];
-                      //$('#table').append("<tr><td>"+place.name+"</td><td>"+place.rating+"</td><td>"+place.types+"</td><td>"+place.id+"</td></tr>");
-
-                      detailRequest = {
-                        placeId: results[i].place_id
-                      };
-
-                      //$('#list').append(results[i].name +  '<br />');
-                      service.getDetails(detailRequest, callback);
-                      function callback(place, status) {
-                        //  console.log(place);
-                        // console.log(place.price_level);
-
-                        //console.log(status);
-
-                        if (status == google.maps.places.PlacesServiceStatus.OK) {
-                          try {
-                            $('#table').append("<tr><td>" + place.name + "</td><td>" + place.rating + "</td><td>" + place.price_level + "</td><td>" + place.url + "</td><td>" + place.opening_hours.open_now + "</td><td>" + place.id + "</td></tr>");
-                          }
-                          catch (e) {
-
-                            console.log("Stupid Happened with obj(s) below")
-                            $('#table').append("<tr><td>" + place.name + "</td><td>" + place.rating + "</td><td>" + place.price_level + "</td><td>" + place.url + "</td><td>Call " + place.international_phone_number + "</td><td>" + place.id + "</td></tr>");
-
-                            console.log(place);
-                            //console.log(e);
-                          }
-                          // $('#list').append(place.name + place.rating + place.url + '<br />');
-                          //console.log(place);
-
-                          //Data for all restaurants within input zip is ready //Ready for another level of complexity
-
-                        }
-                        else if (status == google.maps.places.PlacesServiceStatus.OVER_QUERY_LIMIT) {
-                          setTimeout(i = delayCount(i), 100);   //redo the index which hits the limit , 1000 milisecond = 1 sec
-
-
-                          console.log("Hit Limit Inner ∴ add 1s delay");
-                        }
-
+              service.textSearch(request, searchPlaces);
+              function searchPlaces(results, status) {     //to get up to 60 places
+                      if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        processResults(results);
                       }
 
-                    }
-                    pagination.nextPage();
-                  }  //pagination scope
-                }  //else of processREsults scope
-                paginate++;
-                $('#list').append(`<h4> Total delay ${delay} for ${paginate} Pagination</h4>`);
-                console.log(`Total delay ${delay}`);
-              }  //processResults  scope
-            
+                //console.log(results);
+                // if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                //   return;
+                // } else {
+                //   restaurants.push.apply(restaurants, results);
+                //   if (pagination.hasNextPage) {
+                //     pagination.nextPage();
+
+                //     console.log(results.length);
+
+
+                //   }
+                //   else {
+                //     processResults(restaurants);
+
+                //   }
+                //   //pagination scope
+                // }  //else of search places scope
+                // paginate++;
+                // $('#list').append(`<h4> Total delay ${delay} for ${paginate} Pagination</h4>`);
+                // console.log(`Total delay ${delay}`);
+
+              }  //searchPLaces  scope
             });
 
           }
 
         });
-        
+
+
       });
     }
-  
+
+
   });  //searchbox listener scope
- 
 
 }
 
-function delayCount(i) {
-  i--;
-  delay+=0.1;
-  return i;
+function processResults(restaurants) {
+  var count = 1;
+  
+  $('#list').append(`<h4> Nearest ${restaurants.length} around given address </h4>`);
+
+  for (var i = 0; i < restaurants.length; i++) {
+    service.getDetails({
+      placeId: restaurants[i].place_id
+    }, callback);
+    function callback(place, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        try {
+          // console.log(place)
+
+          $('#table').append("<tr><td>" + count++ + "</td><td>" + place.name + "</td><td>" + place.rating + "</td><td>" + place.price_level + "</td><td>" + place.url + "</td><td>" + place.opening_hours.open_now + "</td><td>" + place.place_id + "</td></tr>");
+        }
+        catch (e) {
+
+          console.log("Stupid Happened with obj(s) below")
+          $('#table').append("<tr><td>" + count++ + "</td><td>" + place.name + "</td><td>" + place.rating + "</td><td>" + place.price_level + "</td><td>" + place.url + "</td><td>Call " + place.international_phone_number + "</td><td>" + place.place_id + "</td></tr>");
+
+          //console.log(place);
+          //console.log(e);
+        }
+        // $('#list').append(place.name + place.rating + place.url + '<br />');
+        //console.log(place);
+
+        //Data for all restaurants within input zip is ready //Ready for another level of complexity
+
+      }
+      else if (status == google.maps.places.PlacesServiceStatus.OVER_QUERY_LIMIT) {
+        setTimeout(1000);
+        i--;   //redo the index which hits the limit , 1000 milisecond = 1 sec
+
+
+      }
+    }
+  }
 }
+
+
+
+//var item = items[Math.floor(Math.random()*items.length)];
 
 // service.nearbySearch({
 //   location: pyrmont,
